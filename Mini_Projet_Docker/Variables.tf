83b63507d678acb973c24fcc717fa6a70490f566
Yo17 <youri.carene@gmail.com>
2024-08-26 23:51:39 +0200

    correction du fichier variables car mauvause synthaxe

--- Mini_Projet_Docker/Variables.tf
+++ Mini_Projet_Docker/Variables.tf
@@ -1,4 +1,35 @@
-network_name          = "custom_network_name"
-volume_name           = "custom_volume_name"
-nginx_container_name  = "custom_nginx_container"
-nginx_external_port   = 8081
+variable "network_name" {
+  description = "Nom du réseau Docker à créer"
+  type        = string
+  default     = "my_custom_network"
+}
+
+variable "volume_name" {
+  description = "Nom du volume Docker à créer"
+  type        = string
+  default     = "nginx_data"
+}
+
+variable "nginx_image" {
+  description = "Nom de l'image Docker pour Nginx"
+  type        = string
+  default     = "nginx:latest"
+}
+
+variable "nginx_container_name" {
+  description = "Nom du conteneur Nginx à créer"
+  type        = string
+  default     = "nginx_container"
+}
+
+variable "nginx_internal_port" {
+  description = "Port interne pour le conteneur Nginx"
+  type        = number
+  default     = 80
+}
+
+variable "nginx_external_port" {
+  description = "Port externe pour le conteneur Nginx"
+  type        = number
+  default     = 8080
+}
