cc7e58668c1788223e4206971c66e205cab304f8
Yo17 <youri.carene@gmail.com>
2024-10-06 22:20:13 +0200

    fichier Docker crée une image Python légère

---
+++ Mini_Projet_Docker/terraform.tf
@@ -0,0 +1,21 @@
+# terraform.tf
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "my_image" {
+  name = "terraform-demo"
+  build {
+    context    = "${path.module}/."
+    dockerfile = "${path.module}/Dockerfile"
+  }
+}
+
+resource "docker_container" "my_container" {
+  image = docker_image.my_image.latest
+  name  = "terraform-demo-container"
+  ports {
+    internal = 8080
+    external = 8080
+  }
+}
